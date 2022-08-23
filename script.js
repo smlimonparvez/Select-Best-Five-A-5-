e0e02e67803955ab52b87a0d38943a75223b6794
smlimonparvez <smlimonparvez@gmail.com>
2022-08-23 18:04:12 +0600

    fifth

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function myFunction() {
 }
 
 /************************************Selected-V*****************************************/
+
 const playerArray = [];
 function display(playerName) {
     const tableBody = document.getElementById('player-list');
@@ -26,18 +27,19 @@ function display(playerName) {
 
 function addToPlayerList(element) {
     const playerName = element.parentNode.parentNode.children[1].innerText;
-    const playerObj = { playerName: playerName }
+    const playerObj = { playerName: playerName };
     playerArray.push(playerObj);
     display(playerArray);
 }
 
 
 /***********************************Budget section*************************************/
-function fivePlayerExpenses() {
+// Per Player Expenses
 
+function fivePlayerExpenses() {
     const perPlayer = document.getElementById('per-player');
     const perPlayerValue = perPlayer.value;
-    const playerExpenses = perPlayerValue * 5;
+    const playerExpenses = perPlayerValue * playerArray.length;
     const totalPlayerExpenses = document.getElementById('player-expenses');
     totalPlayerExpenses.innerText = playerExpenses;
     perPlayer.value = '';
@@ -49,45 +51,37 @@ document.getElementById('calc1-btn').addEventListener('click', function () {
 })
 
 
-document.getElementById('calc2-btn').addEventListener('click', function () {
+// Total Expenses
 
+function managerInput() {
     const manager = document.getElementById('manager-expense');
     const managerExpense = manager.value;
     const managerTotalExpense = parseInt(managerExpense);
+    manager.value = '';
+    return managerTotalExpense
+}
+
+function coachInput() {
     const coach = document.getElementById('coach-expense');
     const coachExpense = coach.value;
     const coachTotalExpense = parseInt(coachExpense);
+    coach.value = '';
+    return coachTotalExpense;
+}
+
+function playerText() {
     const perPlayer = document.getElementById('player-expenses');
     const perPlayerValue = perPlayer.innerText;
-    // const playerExpenses = perPlayerValue * 5;
-
     const playerTotalExpenses = parseInt(perPlayerValue);
-    const subTotal = managerTotalExpense + coachTotalExpense + playerTotalExpenses;
+    return playerTotalExpenses
+}
+
+document.getElementById('calc2-btn').addEventListener('click', function () {
+    const subTotal = playerText() + managerInput() + coachInput();
     const totalExpenses = document.getElementById('total-expenses');
     totalExpenses.innerText = subTotal;
-    manager.value = '';
-    coach.value = '';
 })
 
-// function mg(){
-//     const manager = document.getElementById('manager-expense');
-//     const managerExpense = manager.value;
-//     const managerTotalExpense = parseInt(managerExpense);
-//     manager.value = '';
-//     return managerTotalExpense
-// }
-// function cc(){
-//     const coach = document.getElementById('coach-expense');
-//     const coachExpense = coach.value;
-//     const coachTotalExpense = parseInt(coachExpense);
-//     coach.value = '';
-//     return coachTotalExpense;
-// }
 
-// document.getElementById('calc2-btn').addEventListener('click', function(){
-//     // const subTotal = managerTotalExpense + coachTotalExpense + playerTotalExpenses;
-//     const subTotal = fivePlayerExpenses() + mg() + cc() ;
-//     const totalExpenses = document.getElementById('total-expenses');
-//     totalExpenses.innerText = subTotal;
-// })
+
 
